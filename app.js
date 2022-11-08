a15c668e6ce9b5fba481309044cc60a4a506474d
sijincodes <sijinmathews03@gmail.com>
2022-11-08 23:20:18 +0100

    works added

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
-app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
-app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
-app.get('/quotes', (request, response, next) => response.sendFile(__dirname + '/views/quotes.html'));
+app.use(express.static("public"));
+app.get("/home", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home.html")
+);
+app.get("/about", (request, response, next) =>
+  response.sendFile(__dirname + "/views/about.html")
+);
+app.get("/photo-gallery", (request, response, next) =>
+  response.sendFile(__dirname + "/views/photo-gallery.html")
+);
+app.get("/works", (request, response, next) =>
+  response.sendFile(__dirname + "/views/works.html")
+);
 
-
-app.listen(3000)
-
-
+app.listen(3000);
